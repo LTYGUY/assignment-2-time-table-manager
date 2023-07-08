f1461b92da5b4c3f527d5c7cfdfbcf6ec6edef73
Alywcs <56103017+Alywcs@users.noreply.github.com>
2023-07-08 16:14:43 +0800

    Add files via upload

---
+++ src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.testing;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(MainActivity.this, "Selected date: " + date, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
