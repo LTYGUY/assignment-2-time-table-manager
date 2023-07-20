4850d934d22bf2d57a1067bfcf6472fe8bf8ad84
Alywcs <56103017+Alywcs@users.noreply.github.com>
2023-07-20 20:28:17 +0800

    updated the delete button for listview (previously unable to delete the UI)

--- TimeTable/app/src/main/java/com/example/timetable/ScheduleAdapter.java
+++ TimeTable/app/src/main/java/com/example/timetable/ScheduleAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,6 +16,7 @@ public class ScheduleAdapter extends ArrayAdapter<ScheduleRow> {
     public ScheduleAdapter(Context context, List<ScheduleRow> items) {
         super(context, 0, items);
     }
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -39,13 +42,27 @@ public class ScheduleAdapter extends ArrayAdapter<ScheduleRow> {
             }
         });
 
+        btnDelete.setTag(position);
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int position = (int) v.getTag();
+                ScheduleRow item = getItem(position);
                 AllManagers.DataBaseManager.deleteSchedule(item.ScheduleId);
+                AllManagers.DataBaseManager.deleteSchedule(item.ScheduleId);
+
+                // Now, update the UI by removing the item from the data source
+                // and notifying the adapter about the change.
+                removeItem(position);
+                notifyDataSetChanged();
             }
         });
 
         return convertView;
+    }
+    public void removeItem(int position) {
+        if (position >= 0 && position < getCount()) {
+            remove(getItem(position));
+        }
     }
 }
